f5e09d8feee9c37379a370abbdbfccbc9c77649d
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-12-02 13:58:08 -0500

    make scripts better

--- run.sh
+++ run.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# This script is for local testing. It starts both server and UI in one go.
+
+# gather input parameters
+# if flag -d is set the set LOG_LEVEL to -4 else to 0
+
+while getopts ":d" opt; do
+    case $opt in
+    d)
+        LOG_LEVEL="-4"
+        ;;
+    \?)
+        echo "Invalid option -$OPTARG" >&2
+        ;;
+    esac
+done
+
+if [ -z "${LOG_LEVEL}" ]; then
+    LOG_LEVEL="0"
+fi
+
+echo "LOG_LEVEL = ${LOG_LEVEL}"
+
 export ROOT_DIR=$(pwd)
 
 if [[ "${SAS_TOKEN}" == "" ]]; then
@@ -16,4 +38,4 @@ echo "Storage Account -> ${STORAGE_ACCOUNT_NAME}"
 
 go build -ldflags "-X 'actlabs-auth/entity.SasToken=$SAS_TOKEN' -X 'actlabs-auth/entity.StorageAccountName=$STORAGE_ACCOUNT_NAME'"
 
-redis-cli flushall && export LOG_LEVEL="-4" && export PORT="8882" && ./actlabs-auth
+redis-cli flushall && export LOG_LEVEL="${LOG_LEVEL}" && export PORT="8882" && ./actlabs-auth
