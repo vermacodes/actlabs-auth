fd52881422ae8a3e2e3e8ebc192960e56ba9a475
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-12-02 13:58:54 -0500

    add challenge repository

---
+++ repository/challenge.go
@@ -0,0 +1,144 @@
+package repository
+
+import (
+	"actlabs-auth/entity"
+	"context"
+	"encoding/json"
+	"strings"
+
+	"golang.org/x/exp/slog"
+)
+
+type challengeRepository struct{}
+
+func NewChallengeRepository() entity.ChallengeRepository {
+	return &challengeRepository{}
+}
+
+func (c *challengeRepository) GetAllChallenges() ([]entity.Challenge, error) {
+	challenge := entity.Challenge{}
+	challenges := []entity.Challenge{}
+
+	// URL of the blob with SAS token.
+	serviceClient := getServiceClient().NewClient("Challenges")
+	pager := serviceClient.NewListEntitiesPager(nil)
+	for pager.More() {
+		response, err := pager.NextPage(context.Background())
+		if err != nil {
+			slog.Error("Error getting entities: ", err)
+			return challenges, err
+		}
+
+		for _, element := range response.Entities {
+			//var myEntity aztables.EDMEntity
+			if err := json.Unmarshal(element, &challenge); err != nil {
+				slog.Error("Error unmarshal entity: ", err)
+				return challenges, err
+			}
+			challenges = append(challenges, challenge)
+		}
+	}
+
+	return challenges, nil
+}
+
+func (c *challengeRepository) GetChallengesByLabId(labId string) ([]entity.Challenge, error) {
+	challenge := entity.Challenge{}
+	challenges := []entity.Challenge{}
+
+	// URL of the blob with SAS token.
+	serviceClient := getServiceClient().NewClient("Challenges")
+	pager := serviceClient.NewListEntitiesPager(nil)
+	for pager.More() {
+		response, err := pager.NextPage(context.Background())
+		if err != nil {
+			slog.Error("Error getting entities: ", err)
+			return challenges, err
+		}
+
+		for _, element := range response.Entities {
+			//var myEntity aztables.EDMEntity
+			if err := json.Unmarshal(element, &challenge); err != nil {
+				slog.Error("Error unmarshal entity: ", err)
+				return challenges, err
+			}
+			challenges = append(challenges, challenge)
+		}
+	}
+
+	return challenges, nil
+}
+
+func (c *challengeRepository) GetChallengesByUserId(userId string) ([]entity.Challenge, error) {
+	challenge := entity.Challenge{}
+	challenges := []entity.Challenge{}
+
+	// URL of the blob with SAS token.
+	serviceClient := getServiceClient().NewClient("Challenges")
+	pager := serviceClient.NewListEntitiesPager(nil)
+	for pager.More() {
+		response, err := pager.NextPage(context.Background())
+		if err != nil {
+			slog.Error("Error getting entities: ", err)
+			return challenges, err
+		}
+
+		for _, element := range response.Entities {
+			//var myEntity aztables.EDMEntity
+			if err := json.Unmarshal(element, &challenge); err != nil {
+				slog.Error("Error unmarshal entity: ", err)
+				return challenges, err
+			}
+
+			if challenge.UserId == userId {
+				challenges = append(challenges, challenge)
+			}
+		}
+	}
+
+	return challenges, nil
+}
+
+func (c *challengeRepository) DeleteChallenge(challengeId string) error {
+
+	slog.Debug("Deleting challenge: " + challengeId)
+
+	userId := challengeId[:strings.Index(challengeId, "-")]
+
+	getServiceClient().NewClient("Challenges").DeleteEntity(context.Background(), userId, challengeId, nil)
+	_, err := getServiceClient().NewClient("Challenges").DeleteEntity(context.Background(), userId, challengeId, nil)
+	if err != nil {
+		slog.Error("Error deleting challenge: ", err)
+		return err
+	}
+	slog.Debug("Deleted challenge: " + challengeId)
+
+	return nil
+}
+
+func (c *challengeRepository) UpsertChallenge(challenge entity.Challenge) error {
+	serviceClient := getServiceClient().NewClient("Challenges")
+
+	challenge.PartitionKey = challenge.UserId
+	challenge.RowKey = challenge.ChallengeId
+
+	val, err := json.Marshal(challenge)
+	if err != nil {
+		slog.Error("Error marshalling challenge: ", err)
+		return err
+	}
+
+	_, err = serviceClient.UpsertEntity(context.Background(), val, nil)
+	if err != nil {
+		slog.Error("Error updating/creating challenge: ", err)
+		return err
+	}
+
+	slog.Debug("Challenge updated/created successfully")
+
+	return nil
+}
+
+func (c *challengeRepository) ValidateUser(userId string) (bool, error) {
+	return true, nil
+}
